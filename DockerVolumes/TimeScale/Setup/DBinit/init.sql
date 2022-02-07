1a1f928dc5186c5b48b514e604deb7031478012c
Nathan <nathan.bertholier@gmail.com>
2022-02-07 11:41:19 +0100

    Patch SQL Docker

--- DockerVolumes/TimeScale/Setup/DBinit/init.sql
+++ DockerVolumes/TimeScale/Setup/DBinit/init.sql
@@ -4,3 +4,4 @@ CREATE TABLE report(id bigint PRIMARY KEY,content text NOT NULL,proximity text N
 CREATE TABLE token(id bigserial PRIMARY KEY,idlog bigint NOT NULL,idtokentype int NOT NULL,value text not null);
 CREATE TABLE tokentype(id serial PRIMARY KEY,name varchar(20) NOT NULL);
 INSERT INTO tokentype (name) values ('IPV4'),('IPV6'),('Statut'),('Datetime'),('EdgeResponse');
+
