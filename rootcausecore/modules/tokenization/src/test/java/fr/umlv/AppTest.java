c2fd306568bfa7c338b13f9b80687aa20d4aeb5e
Nathan <nathan.bertholier@gmail.com>
2022-02-07 12:30:16 +0100

    NB-RC-54 Cas de test DateTime,IPV4,IPV6

--- rootcausecore/modules/tokenization/src/test/java/fr/umlv/AppTest.java
+++ rootcausecore/modules/tokenization/src/test/java/fr/umlv/AppTest.java
@@ -2,19 +2,32 @@ package fr.umlv;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.
+                    getConnection("jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'target/test-classes/lightdata.sql'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void tokenizationDateTime() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM datetime");
+        final ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            assertEquals(resultSet.getString(2), resultSet.getString(1));
+        }
     }
 }
