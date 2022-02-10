0c9dd29e218f31fa9f0f4fcf45a87fd1c157da43
Nefarum <guillemaudyann@gmail.com>
2022-02-10 10:27:19 +0100

    Overriden record methods

--- core/src/main/java/fr/uge/modules/api/server/external/model/Proximity.java
+++ core/src/main/java/fr/uge/modules/api/server/external/model/Proximity.java
@@ -1,5 +1,24 @@
 package fr.uge.modules.api.server.external.model;
 
-public record Proximity(long id_target, Link[] links) {
+import java.util.Arrays;
 
+public record Proximity(long id_target, Link[] links) {
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Proximity proximity)) return false;
+        else return proximity.id_target == id_target && Arrays.equals(proximity.links, links);
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id_target) ^ Arrays.hashCode(links);
+    }
+
+    @Override
+    public String toString() {
+        return "Proximity{" +
+                "id_target=" + id_target +
+                ", links=" + Arrays.toString(links) +
+                '}';
+    }
 }
