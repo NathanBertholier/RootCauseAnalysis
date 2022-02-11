e6090d349721bc14a19160f98c68f99237eef8da
Nicolas <nicolas.fortune@live.Fr>
2022-02-11 09:33:26 +0100

    Adding the treatment of status in the Tokenization. Adding tests for the tokenization using h2 with a new sql file.

---
+++ core/src/test/java/fr/uge/modules/tokenization/TokenizationTest.java
@@ -0,0 +1,37 @@
+package fr.uge.modules.tokenization;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.sql.*;
+
+class TokenizationTest {
+
+    private static Connection connection;
+    private Tokenization tokenization = new Tokenization();
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'src/test/resources/lightdata.sql'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void tokenization() throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM rawlog");
+        final ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            assertEquals(tokenization.tokenizeLog(resultSet.getString(1)).toString(), resultSet.getString(2));
+        }
+    }
+
+    @Test
+    void shouldThrowNullPointerException(){
+        assertThrows(NullPointerException.class, () -> {
+            tokenization.tokenizeLog(null);
+        });
+    }
+
+}
