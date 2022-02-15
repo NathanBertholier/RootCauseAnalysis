0a70dbfdaef1bc4ea3d1374e5af134f9afe8b5db
Nathan <nathan.bertholier@gmail.com>
2022-02-15 16:31:40 +0100

    update test

--- core/src/test/java/fr/uge/modules/api/TokenTypesTest.java
+++ core/src/test/java/fr/uge/modules/api/TokenTypesTest.java
@@ -1,12 +1,8 @@
 package fr.uge.modules.api;
 
-import fr.uge.modules.api.server.external.token.TokenType;
-import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
