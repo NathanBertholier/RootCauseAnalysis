00ca58a34f891b19a00495ecd8b644891db37398
Nefarum <guillemaudyann@gmail.com>
2022-02-16 16:49:16 +0100

    Updated report endpoint

--- core/src/main/java/fr/uge/modules/api/endpoint/report/Report.java
+++ core/src/main/java/fr/uge/modules/api/endpoint/report/Report.java
@@ -5,6 +5,7 @@ import fr.uge.modules.api.model.report.ReportParameter;
 import fr.uge.modules.synthetization.Synthetization;
 import io.smallrye.mutiny.Uni;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,12 +17,18 @@ public class Report {
     private static final Logger LOGGER = Logger.getLogger(Report.class.getName());
 
     @GET
-    @Consumes(APPLICATION_JSON)
     @Produces(APPLICATION_JSON)
-    public Uni<ReportResponse> getReport(@PathParam("id")long idLogTarget,@QueryParam("expanded") boolean expanded,@QueryParam("delta") long delta,@QueryParam("cache") boolean cache,@QueryParam("proximity_limit") float proximity_limit,@QueryParam("network_size") int network_size) throws SQLException {
+    public Uni<Response> getReport(
+            @PathParam("id") long idLogTarget,
+            @QueryParam("expanded") boolean expanded,
+            @QueryParam("delta") long delta,
+            @QueryParam("cache") boolean cache,
+            @QueryParam("proximity_limit") float proximity_limit,
+            @QueryParam("network_size") int network_size
+    ) throws SQLException {
         ReportParameter reportParameter = new ReportParameter(expanded, delta, cache, proximity_limit, network_size);
         LOGGER.log(Level.INFO, "Received request for id " +  idLogTarget + " with parameters: " + reportParameter);
         var report = Synthetization.getReport(idLogTarget, reportParameter);
-        return Uni.createFrom().item(report);
+        return Uni.createFrom().item(Response.ok().entity("Response incoming").build());
     }
 }
