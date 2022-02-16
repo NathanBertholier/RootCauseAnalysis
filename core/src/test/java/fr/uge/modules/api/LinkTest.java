c9c9940d92918c7ae3e8150fdaabf009307411e6
Niamor2012 <weber.romain@laposte.net>
2022-02-16 11:35:54 +0100

    add test link

---
+++ core/src/test/java/fr/uge/modules/api/LinkTest.java
@@ -0,0 +1,24 @@
+package fr.uge.modules.api;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.vertx.core.json.JsonObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
+@QuarkusTest
+public class LinkTest {
+    @Test
+    public void linkShouldReturnCorrectJSON() {
+        given()
+                .when().get("http://localhost:8080/link?delta=7200&id1=2&id2=3")
+                .then()
+                .statusCode(200)
+                .body("computations[0].token_type",is("type"))
+                .body("computations[0].value_log_first",is("value1"))
+                .body("computations[0].value_log_second",is("value2"))
+                .body("computations[0].proximity",is(1.0F))
+                .body("proximity",is(1.05F));
+    }
+}
