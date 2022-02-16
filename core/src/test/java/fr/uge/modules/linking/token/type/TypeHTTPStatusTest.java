28ea589a0900f15e63d1bf2111cb35f59f93f4ec
Niamor2012 <weber.romain@laposte.net>
2022-02-16 15:55:40 +0100

    add status test

--- core/src/test/java/fr/uge/modules/linking/token/type/TypeHTTPStatusTest.java
+++ core/src/test/java/fr/uge/modules/linking/token/type/TypeHTTPStatusTest.java
@@ -2,31 +2,34 @@ package fr.uge.modules.linking.token.type;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TypeHTTPStatusTest {
     @Test
+    void nullValue() {
+        TypeHTTPStatus typeHTTPStatus = new TypeHTTPStatus();
+        assertThrows(NullPointerException.class,() -> typeHTTPStatus.matcher(null));
+    }
+    @Test
     void regexMatchValues() {
         TypeHTTPStatus typeHTTPStatus = new TypeHTTPStatus();
-        assertAll(
-                () -> assertTrue("Hit".matches(typeHTTPStatus.getRegex())),
-                () -> assertTrue("RefreshHit".matches(typeHTTPStatus.getRegex())),
-                () -> assertTrue("Miss".matches(typeHTTPStatus.getRegex())),
-                () -> assertTrue("LimitExceeded".matches(typeHTTPStatus.getRegex())),
-                () -> assertTrue("CapacityExceeded".matches(typeHTTPStatus.getRegex())),
-                () -> assertTrue("Error".matches(typeHTTPStatus.getRegex())),
-                () -> assertTrue("Redirect".matches(typeHTTPStatus.getRegex())));
+        Random r = new Random();
+        for(int i=0;i<10;i++){
+            var status = ""+r.nextInt(1,6)+r.nextInt(0,6)+r.nextInt(0,10);
+            assertEquals(typeHTTPStatus.getTokenTypeId(),typeHTTPStatus.matcher(status));
+        }
     }
 
     @Test
     void regexNotMatchValue() {
         TypeHTTPStatus typeHTTPStatus = new TypeHTTPStatus();
-        assertAll(
-                () -> assertFalse("HIt".matches(typeHTTPStatus.getRegex())),
-                () -> assertFalse("RefreshHitt".matches(typeHTTPStatus.getRegex())),
-                () -> assertFalse("Mess".matches(typeHTTPStatus.getRegex())),
-                () -> assertFalse("Limit Exceeded".matches(typeHTTPStatus.getRegex())),
-                () -> assertFalse("Capacity_Exceeded".matches(typeHTTPStatus.getRegex())));
+        Random r = new Random();
+        for(int i=0;i<10;i++){
+            var status = ""+r.nextInt(0,10)+r.nextInt(6,10)+r.nextInt(0,10);
+            assertEquals(-1,typeHTTPStatus.matcher(status));
+        }
     }
 }
