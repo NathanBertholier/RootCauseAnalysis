5ef3c56ebb666994cfd9b1b164f7b1dba19f4b66
Nathan <nathan.bertholier@gmail.com>
2022-02-16 15:38:25 +0100

    update proximity computation

--- core/src/main/java/fr/uge/modules/linking/token/type/TypeDate.java
+++ core/src/main/java/fr/uge/modules/linking/token/type/TypeDate.java
@@ -5,7 +5,7 @@ import fr.uge.modules.api.model.TokenModel;
 public class TypeDate implements TokenType{
 
     private static final String NAME = "date";
-    private static final String REGEX = "((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])";
+    private static final String REGEX = "([0-9]{4})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])";
 
     @Override
     public String getName() {
