ba2f3ca0d2933b8e0c61052787e23b50cc820a4c
Nefarum <guillemaudyann@gmail.com>
2022-02-17 15:51:16 +0100

    Added environment retriever for default value, updated report endpoint & report parameter with environment retriever

--- core/src/main/java/fr/uge/modules/api/endpoint/report/Report.java
+++ core/src/main/java/fr/uge/modules/api/endpoint/report/Report.java
@@ -1,9 +1,12 @@
 package fr.uge.modules.api.endpoint.report;
 
-import fr.uge.modules.api.model.ReportResponse;
+import fr.uge.modules.api.EnvRetriever;
 import fr.uge.modules.api.model.report.ReportParameter;
 import fr.uge.modules.synthetization.Synthetization;
 import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
@@ -14,21 +17,30 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/report/{id}")
 public class Report {
+    @Inject
+    EnvRetriever envRetriever;
     private static final Logger LOGGER = Logger.getLogger(Report.class.getName());
 
     @GET
     @Produces(APPLICATION_JSON)
     public Uni<Response> getReport(
             @PathParam("id") long idLogTarget,
-            @QueryParam("expanded") boolean expanded,
-            @QueryParam("delta") long delta,
-            @QueryParam("cache") boolean cache,
-            @QueryParam("proximity_limit") float proximity_limit,
-            @QueryParam("network_size") int network_size
+            @QueryParam("expanded") Boolean expanded,
+            @QueryParam("delta") Long delta,
+            @QueryParam("cache") Boolean cache,
+            @QueryParam("proximity_limit") Integer proximity_limit,
+            @QueryParam("network_size") Integer network_size
     ) throws SQLException {
+        if(expanded == null) expanded = envRetriever.reportDefaultExpanded();
+        if(delta == null) delta = envRetriever.reportDefaultDelta();
+        if(cache == null) cache = envRetriever.reportDefaultCache();
+        if(proximity_limit == null) proximity_limit = envRetriever.reportDefaultLimit();
+        if(network_size == null) network_size = envRetriever.reportDefaultSize();
+
         ReportParameter reportParameter = new ReportParameter(expanded, delta, cache, proximity_limit, network_size);
         LOGGER.log(Level.INFO, "Received request for id " +  idLogTarget + " with parameters: " + reportParameter);
+        System.out.println("ReportParameter: " + reportParameter);
         var report = Synthetization.getReport(idLogTarget, reportParameter);
-        return Uni.createFrom().item(Response.ok().entity("Response incoming").build());
+        return Uni.createFrom().item(Response.ok().entity(reportParameter).build());
     }
 }
