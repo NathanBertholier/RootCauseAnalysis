634cb685211311627c43274e2b1656a7a5f26493
Yi sa ZHENG <yisa.zheng@outlook.com>
2022-02-23 15:40:59 +0100

    edgeResponse proximity calc

--- core/src/main/java/fr/uge/modules/linking/token/type/TypeEdgeResponse.java
+++ core/src/main/java/fr/uge/modules/linking/token/type/TypeEdgeResponse.java
@@ -1,6 +1,5 @@
 package fr.uge.modules.linking.token.type;
 
-import fr.uge.modules.api.model.TokenModel;
 import fr.uge.modules.api.model.entities.TokenEntity;
 
 import java.util.List;
@@ -28,12 +27,26 @@ public class TypeEdgeResponse implements TokenType {
 
     @Override
     public float computeProximity(List<TokenEntity> tokenLeft, List<TokenEntity> tokenRight) {
-        return 0;/*
-        if (t1.equals(t2)) {
-            return 100;
-        } else if (ERRORS.contains(t1.token_value()) && ERRORS.contains(t2.token_value())) {
-            return 50;
+        if ( tokenLeft.isEmpty() || tokenRight.isEmpty() ) return 50;
+
+        float proximity = 0;
+        for (TokenEntity tokenL : tokenLeft) {
+            String leftValue = tokenL.getValue();
+            proximity += tokenRight.stream().mapToLong( tokenR -> {
+                String rightValue = tokenR.getValue();
+                if ( leftValue.equals( rightValue ) ) {
+                    return 100;
+                }
+                else if ( (( ERRORS.contains( leftValue ) && ERRORS.contains( rightValue ) ) || ( !ERRORS.contains( leftValue ) && !ERRORS.contains( rightValue ) ) ) ) {
+                    return 95;
+                }
+                else if ( !ERRORS.contains( leftValue ) && ERRORS.contains( rightValue ) ) {
+                    return 25;
+                }
+                return 0;
+            } ).reduce(0, Long::max);
         }
-        return 0;*/
+
+        return proximity / tokenLeft.size();
     }
 }
