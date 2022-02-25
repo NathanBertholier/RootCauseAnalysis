03fd4544a327a9e02ec71b24c9ccd8fc393626d4
Kappawaii <32612956+Kappawaii@users.noreply.github.com>
2022-02-25 08:39:06 +0100

    updated test arguments

--- tools/benchmark/src/main/java/fr/umlv/rootcause/benchmark/Benchmark.java
+++ tools/benchmark/src/main/java/fr/umlv/rootcause/benchmark/Benchmark.java
@@ -18,9 +18,9 @@ import java.util.stream.Stream;
 public class Benchmark {
 
     public static void main(String[] args) throws ParseException, IOException, InterruptedException {
-        //use this to try out spamming rabbitmq
-        //args = new String[]{"-l", "100000", "-F", "../logs/", "-b", "50", "-d", "85", "-t", "8", "-u", "http://localhost:80/external/insertlog/batch", "-nT", "2022-02-18 11:57:00"};
-        //args = new String[]{"-l", "100000", "-F", "../logs/", "-b", "50", "-d", "85", "-t", "8", "-u", "http://localhost:80/external/insertlog/batch"};
+        //use this to try out spamming rabbitmq with different parameters
+        //args = new String[]{"-l", "100000", "-F", "../logs/", "-b", "50", "-d", "85", "-t", "8", "-u", "http://localhost:8081/insertlog", "-nT", "2022-02-18 11:57:00"};
+        //args = new String[]{"-l", "100000", "-F", "../logs/", "-b", "50", "-d", "85", "-t", "8", "-u", "http://localhost:8081/insertlog"};
         //args = new String[]{"-l", "100000", "-F", "../logs/", "-b", "50", "-d", "85", "-t", "8", "-u", "http://localhost:8081/insertlog","-lo","-s","stats.csv"};
 
         //use this to output to out.txt in exec folder
