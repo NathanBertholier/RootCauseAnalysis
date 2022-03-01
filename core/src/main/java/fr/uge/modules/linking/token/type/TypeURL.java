736e363f743dde3d1f32b385eb3c49f634c4162a
Nefarum <guillemaudyann@gmail.com>
2022-03-01 11:01:14 +0100

    Updated typeurl computation

--- core/src/main/java/fr/uge/modules/linking/token/type/TypeURL.java
+++ core/src/main/java/fr/uge/modules/linking/token/type/TypeURL.java
@@ -1,8 +1,11 @@
 package fr.uge.modules.linking.token.type;
 
 import fr.uge.modules.api.model.entities.TokenEntity;
+import fr.uge.modules.api.model.linking.Computation;
+import fr.uge.modules.api.model.linking.Links;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class TypeURL implements TokenType{
@@ -24,35 +27,38 @@ public class TypeURL implements TokenType{
     }
 
     @Override
-    public double computeProximity(List<TokenEntity> tokenLeft, List<TokenEntity> tokenRight) {
+    public Links computeProximity(List<TokenEntity> tokenLeft, List<TokenEntity> tokenRight) {
         if(tokenLeft.isEmpty() || tokenRight.isEmpty()) {
-            return 50;
+            Links.emptyLink(50);
         }
 
         String regex = "^(http[s]?://www\\.|http[s]?://|www\\.)";
 
-        return tokenLeft.stream().map(token -> token.getValue().replaceFirst(regex, ""))
-                .mapToDouble(tokenL -> tokenRight.stream()
+        var computations = tokenLeft.stream().map(token -> token.getValue().replaceFirst(regex, ""))
+                .map(tokenL -> tokenRight.stream()
                 .map(token -> token.getValue().replaceFirst(regex, "") )
-                .mapToDouble(tokenR -> {
-            if ( tokenL.equals( tokenR ) ) {
-                return 100;
-            }
+                .map(tokenR -> {
+                    if ( tokenL.equals( tokenR ) ) {
+                        return new Computation(this, tokenL, tokenR, 100d);
+                    }
 
-            var arrayL = tokenL.split( "/" );
-            var arrayR = tokenR.split( "/" );
-            double count = 0;
-            for ( int i =0; i < arrayL.length-1; i++ ) {
-                if(i<= arrayR.length-1){
-                    if ( arrayL[i].equals( arrayR[i] ) ) {
-                        count++;
+                    var arrayL = tokenL.split( "/" );
+                    var arrayR = tokenR.split( "/" );
+                    double count = 0;
+                    for ( int i =0; i < arrayL.length-1; i++ ) {
+                        if(i<= arrayR.length-1){
+                            if ( arrayL[i].equals( arrayR[i] ) ) {
+                                count++;
+                            }
+                            else {
+                                break;
+                            }
+                        }
                     }
-                    else {
-                        break;
-                    }
-                }
-            }
-            return (count / arrayL.length) * 100;
-        } ).reduce( 0,Double::max)).reduce(0, Double::sum) / tokenLeft.size();
+                    return new Computation(this, tokenL, tokenR, (count / arrayL.length) * 100);
+                }).toList())
+                .flatMap(Collection::stream)
+                .toList();
+        return new Links(computations, computations.stream().mapToDouble(Computation::proximity).sum() / computations.size());
     }
 }
