4e6d277f543b719b8301f958b2edba05098fe3c8
Nefarum <guillemaudyann@gmail.com>
2022-03-09 11:22:09 +0100

    Adding javadoc

--- core/src/main/java/fr/uge/modules/error/AbstractRootCauseError.java
+++ core/src/main/java/fr/uge/modules/error/AbstractRootCauseError.java
@@ -2,6 +2,10 @@ package fr.uge.modules.error;
 
 import javax.ws.rs.core.Response;
 
+/**
+ * Abstract object representing a custom RootCause error, extending RuntimeException in order to throw those
+ * without modifying method's signature with classic throwable errors.
+ */
 public abstract class AbstractRootCauseError extends RuntimeException implements RootCauseError {
     AbstractRootCauseError() {
         super();
--- core/src/main/java/fr/uge/modules/error/NotYetTokenizedError.java
+++ core/src/main/java/fr/uge/modules/error/NotYetTokenizedError.java
@@ -1,5 +1,10 @@
 package fr.uge.modules.error;
 
+/**
+ * Class representing a non-found log inside the DB
+ * This error can mean two possible situation: the specific log has never been inserted,
+ * or might be waiting for its tokenization
+ */
 public class NotYetTokenizedError extends AbstractRootCauseError {
     @Override
     public int getStatus() {
