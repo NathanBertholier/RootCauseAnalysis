56139395884eeae4772c756be4bf51455b8c81e4
Floflo <o@o>
2022-03-09 11:01:54 +0100

    Adding management of errors on TokenEndpoint

--- core/src/main/java/fr/uge/modules/api/endpoint/token/TokenEndpoint.java
+++ core/src/main/java/fr/uge/modules/api/endpoint/token/TokenEndpoint.java
@@ -1,7 +1,6 @@
 package fr.uge.modules.api.endpoint.token;
 
 import fr.uge.modules.api.model.TokenRequest;
-import fr.uge.modules.error.NotYetTokenizedError;
 import fr.uge.modules.tokenization.TokenRetriever;
 import io.smallrye.mutiny.Uni;
 
@@ -11,10 +10,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static fr.uge.modules.api.serializer.TokenResponseSerializer.*;
-import static fr.uge.modules.error.AbstractRootCauseError.fromError;
 
 @Path("/tokens")
 public class TokenEndpoint {
@@ -24,14 +27,30 @@ public class TokenEndpoint {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> getTokens(TokenRequest tokenRequest) {
-
-        return TokenRetriever.getTokens(tokenRequest)
-                .map(logs -> new TokensResponse(logs, null))
+        return testDateTime(tokenRequest.init_datetime(), tokenRequest.end_datetime())
+                .orElse(TokenRetriever.getTokens(tokenRequest)
+                .map(logs -> new TokensResponse(logs, ""))
                 .map(tokensResponse -> Response.ok(tokensResponse).build())
                 .onFailure()
-                .recoverWithItem(Response.ok(new TokensResponse(null, "Token not yet tokenized")).build())
+                .recoverWithItem(Response.ok(new TokensResponse(new ArrayList<>(), "Log no found with id : " + tokenRequest.id())).build())
                 .onFailure().invoke(
                         error -> LOGGER.severe(() -> "Error while retrieving tokens: " + error)
-                );
+                ));
+    }
+
+    private Optional<Uni<Response>> testDateTime(String initDatetime, String endDatetime) {
+        if(!initDatetime.isEmpty() && !endDatetime.isEmpty()) {
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd h:m:s");
+                if(!sdf.parse(initDatetime).before(sdf.parse(endDatetime))) {
+                    return Optional.of(Uni.createFrom()
+                            .item(Response.ok(new TokensResponse(new ArrayList<>(), "The start date cannot be greater than the end date."))
+                                    .build()));
+                }
+            } catch (ParseException e) {
+                LOGGER.log(Level.SEVERE, "Error while parsing date in tokenEndPoint", e);
+            }
+        }
+        return Optional.empty();
     }
 }
