ed19d525166797d911b53b53a3de4bcd11e73ddb
Nefarum <guillemaudyann@gmail.com>
2022-03-09 11:09:09 +0100

    Adding javadoc

--- core/src/main/java/fr/uge/modules/api/model/TokenModel.java
+++ core/src/main/java/fr/uge/modules/api/model/TokenModel.java
@@ -1,7 +1,7 @@
 package fr.uge.modules.api.model;
 
 /**
- * Record representing a Tokens request, containing the specific type of requested tokens and their values
+ * Record used to spell out a Tokens request, containing the specific type of requested tokens and their values
  */
 public record TokenModel(String token_type, String token_value) {
 }
--- core/src/main/java/fr/uge/modules/api/model/TokenRequest.java
+++ core/src/main/java/fr/uge/modules/api/model/TokenRequest.java
@@ -2,5 +2,14 @@ package fr.uge.modules.api.model;
 
 import java.util.List;
 
+/**
+ * Record representing a Tokens request according to the given parameters.
+ * A TokenRequest contains the following parameters:
+ * - init_datetime, a string representing the start time of the search
+ * - end_datetime, a string representing the end time of the search
+ * - id, a long value in the case the user wants to search tokens of a specific log
+ * - tokens, a list a tokenmodel used to spell out the request
+ * - rows, an int value representing the maximum number of the database queried logs
+ */
 public record TokenRequest(String init_datetime, String end_datetime, long id, List<TokenModel> tokens, int rows) {
 }
